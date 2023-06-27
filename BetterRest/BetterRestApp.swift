38502bf5c0dd176263e6cd0186f3a781520eca21
amiller5233@gmail.com <amiller5233@gmail.com>
2023-06-26 20:32:03 -0700

    Create xcode project

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Adam Miller on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
